feda0bc9abbe7900c7aa883647eceb1ac4c13149
Gas Com IT <99112362+GasComIT@users.noreply.github.com>
2022-04-05 08:17:20 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.use(express.static(__dirname))
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+app.listen(3000, () => {
+  console.log('Powered By GasComIT');
+});
